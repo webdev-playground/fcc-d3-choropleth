788d07a258abfd2bb9bde4675549c00d7393479e
Josh Choo <dev@joshuous.com>
2020-02-09 22:35:15 +0800

    create svg, and draw a map based on counties data

---
+++ app.js
@@ -0,0 +1,29 @@
+const width = 1000;
+const height = 1100;
+
+const svg = d3
+  .select('#map')
+  .append('svg')
+  .attr('width', width)
+  .attr('height', height);
+
+const path = d3.geoPath();
+
+d3.json(
+  'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json'
+)
+  .then(countiesData => {
+    // convert data from topojson to geojson format
+    const geojson = topojson.feature(
+      countiesData,
+      countiesData.objects.counties
+    );
+    console.log(geojson.features);
+    svg
+      .selectAll('path')
+      .data(geojson.features)
+      .enter()
+      .append('path')
+      .attr('d', path);
+  })
+  .catch(() => alert('An error occurred!'));
